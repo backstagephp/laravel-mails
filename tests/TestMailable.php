40d51f743758adc858ddaf5d326a8c140c1658b9
Mark van Eijk <mark@ux.nl>
2026-03-26 21:22:35 +0100

    wip

---
+++ tests/TestMailable.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+
+class TestMailable extends Mailable
+{
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            from: 'from@example.com',
+            subject: 'Test Mailable',
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            htmlString: '<p>Test HTML content</p>',
+        );
+    }
+}
