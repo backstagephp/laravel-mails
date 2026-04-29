7eb296f1779519e790d4b198a9c2054e9f4b0f7f
Mark van Eijk <mark@ux.nl>
2026-04-29 22:05:00 +0200

    add tags tests

---
+++ tests/Fixtures/TaggedMailable.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Backstage\Mails\Tests\Fixtures;
+
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+
+class TaggedMailable extends Mailable
+{
+    /**
+     * @param  array<int, string>  $envelopeTags
+     */
+    public function __construct(public array $envelopeTags) {}
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Tagged subject',
+            tags: $this->envelopeTags,
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(htmlString: '<p>Tagged HTML</p>');
+    }
+}
--- tests/MailLogTest.php
+++ tests/MailLogTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Backstage\Mails\Models\Mail as MailModel;
+use Backstage\Mails\Tests\Fixtures\TaggedMailable;
 use Illuminate\Mail\Message;
 use Illuminate\Support\Facades\Mail;
 
@@ -8,7 +9,7 @@ use function Pest\Laravel\assertDatabaseHas;
 
 it('can log sent mails', function (): void {
     Mail::send([], [], function (Message $message): void {
-        $message->to('mark@vormkracht10.nl')
+        $message->to('mark@ux.nl')
             ->from('local@computer.nl')
             ->cc('cc@vk10.nl')
             ->bcc('bcc@vk10.nl')
@@ -19,11 +20,40 @@ it('can log sent mails', function (): void {
 
     assertDatabaseHas((new MailModel)->getTable(), [
         'from' => json_encode(['local@computer.nl' => null]),
-        'to' => json_encode(['mark@vormkracht10.nl' => null]),
+        'to' => json_encode(['mark@ux.nl' => null]),
         'cc' => json_encode(['cc@vk10.nl' => null]),
         'bcc' => json_encode(['bcc@vk10.nl' => null]),
         'subject' => 'Test',
         'html' => '<p>HTML</p>',
         'text' => 'Text',
+    ]);
+});
+
+it('logs tags from a mailable envelope', function (): void {
+    config()->set('mails.logging.attributes', array_merge(
+        (array) config('mails.logging.attributes'),
+        ['tags'],
+    ));
+
+    Mail::to('mark@ux.nl')
+        ->send(new TaggedMailable(['Campaign:42', 'audience:newsletter']));
+
+    assertDatabaseHas((new MailModel)->getTable(), [
+        'subject' => 'Tagged subject',
+        'tags' => json_encode(['Campaign:42', 'audience:newsletter']),
+    ]);
+});
+
+it('logs an empty tag list when the envelope has no tags', function (): void {
+    config()->set('mails.logging.attributes', array_merge(
+        (array) config('mails.logging.attributes'),
+        ['tags'],
+    ));
+
+    Mail::to('mark@ux.nl')->send(new TaggedMailable([]));
+
+    assertDatabaseHas((new MailModel)->getTable(), [
+        'subject' => 'Tagged subject',
+        'tags' => json_encode([]),
     ]);
 });
